6ce9825c0c671739292342f668da6ad69835463c
Wang Dachui <79186819+FF-crazy@users.noreply.github.com>
2024-11-05 15:39:44 +0800

    AList completed

--- practice/AList.java
+++ practice/AList.java
@@ -1,4 +1,4 @@
-public class AList<type> {
+public class AList<type> implements List<type> {
 
   private type[] items;
   private int size;
@@ -21,11 +21,18 @@ public class AList<type> {
       System.arraycopy(items, 0, n, 0, size / 2);
       items = n;
       return true;
+    } else {
+      return false;
     }
-    else return false;
   }
 
-  public void addLast(type x) {
+  @Override
+  public int getLength() {
+    return size;
+  }
+
+  @Override
+  public void append(type x) {
     if (size == items.length) {
       resize(size * 2);
     }
@@ -33,6 +40,77 @@ public class AList<type> {
     size++;
   }
 
+  @Override
+  public void insert(type x) {
+    type[] newarray = (type[]) new Object[size + 1];
+    newarray[0] = x;
+    System.arraycopy(items, 0, newarray, 1, size);
+    items = newarray;
+    size++;
+  }
 
+  @Override
+  public void insert(type x, int position) {
+    type[] newarray = (type[]) new Object[size + 1];
+    newarray[position] = x;
+    System.arraycopy(items, 0, newarray, 0, position);
+    System.arraycopy(items, position, newarray, position + 1, size - position);
+    items = newarray;
+    size++;
+  }
+
+  @Override
+  public type getFirst() {
+    return items[0];
+  }
+
+  @Override
+  public void printList() {
+    for (int i = 0; i < size; i++) {
+      System.out.print(items[i] + " ");
+    }
+    System.out.println();
+  }
+
+  @Override
+  public void deleteElement(type item) {
+    int i = 0;
+    while (items[i] != item) {
+      i++;
+    }
+    type[] newarray = (type[]) new Object[size];
+    System.arraycopy(items, i + 1, newarray, 0, size - i - 1);
+    System.arraycopy(newarray, 0, items, i, size - i);
+    size--;
+  }
+
+  @Override
+  public void deletePosition(int i) {
+    type[] newarray = (type[]) new Object[size];
+    System.arraycopy(items, i + 1, newarray, 0, size - i - 1);
+    System.arraycopy(newarray, 0, items, i, size - i);
+    size--;
+  }
+
+  @Override
+  public void changeByPosition(type item, int pos) {
+    items[pos] = item;
+  }
+
+  @Override
+  public type checkByPosition(int pos) {
+    return items[pos];
+  }
+
+  @Override
+  public int checkByElement(type item) {
+    int i = 0;
+    while (items[i] != item) {
+      i++;
+    }
+    return i;
+  }
 
 }
+
+
--- practice/UnitTest.java
+++ practice/UnitTest.java
@@ -1,12 +1,25 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 public class UnitTest {
 
+//  @Test
+//  public void DllistTest() {
+//    int[] input = new int[]{1, 2, 3, 4};
+//    int[] expect = new int[]{2, 3, 4, 6};
+//    Assert.assertArrayEquals(input, expect);
+//  }
+
   @Test
-  public void DllistTest() {
-    int[] input = new int[]{1, 2, 3, 4};
-    int[] expect = new int[]{2, 3, 4, 6};
-    Assert.assertArrayEquals(input, expect);
+  public void AListTest() {
+    List<Integer> list = new AList<Integer>();
+    list.append(1);
+    list.insert(0);
+    list.insert(2, 1);
+    list.insert(100, 2);
+    list.printList();
+    list.deleteElement(100);
+    list.printList();
+    System.out.println(list.checkByPosition(2));
+    System.out.println(list.checkByElement(1));
   }
 }
