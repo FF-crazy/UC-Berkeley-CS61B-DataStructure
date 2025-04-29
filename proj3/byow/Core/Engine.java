dad84e7f100b43f2bda17e1642555bdc879f83e9
FF_Crazy <13453488571@163.com>
2025-04-29 19:50:08 +0800

    first version

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -6,20 +6,26 @@ import byow.TileEngine.Tileset;
 import edu.princeton.cs.introcs.StdDraw;
 import java.awt.Color;
 import java.awt.Font;
+import java.util.Queue;
 import java.util.Random;
 
 public class Engine {
+
   private TERenderer ter;
   /* Feel free to change the width and height. */
   public static final int WIDTH = 50;
   public static final int HEIGHT = 50;
   private Random random;
   private TETile[][] tetile;
+  private int width;
+  private int height;
 
 
   public Engine() {
     ter = new TERenderer();
+    ter.initialize(WIDTH, HEIGHT);
   }
+
   /**
    * Method used for autograding and testing your code. The input string will be a series of
    * characters (for example, "n123sswwdasdassadwas", "n123sss:q", "lwww". The engine should behave
@@ -45,17 +51,45 @@ public class Engine {
     //
     // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
     // that works for many different input types.
-    initialize(input);
-
-
+    boolean flag = initialize(input);
+    if (!flag) {
+      System.out.println("Undefined Behavior!\n");
+      return;
+    }
+    width = RandomUtils.uniform(random, WIDTH / 5 * 4, WIDTH-1);
+    height = RandomUtils.uniform(random, HEIGHT / 5 * 4, HEIGHT-1);
+    createBound(width + 1, height + 1);
+    divide(0, 0, width+1, height+1);
+    bfs();
+    ter.renderFrame(tetile);
   }
-  private void initialize(String input) {
+
+  /**
+   * Method used for exploring a fresh world. This method should handle all inputs, including inputs
+   * from the main menu.
+   */
+  public void interactWithKeyboard() {
+    Menu menu = new Menu(WIDTH, HEIGHT);
+    String input = menu.freshAndGetString();
+    menu.close();
+    boolean flag = initialize(input);
+    if (!flag) {
+      System.out.println("Undefined Behavior!\n");
+      return;
+    }
+    width = RandomUtils.uniform(random, WIDTH / 3 * 2, WIDTH);
+    height = RandomUtils.uniform(random, HEIGHT / 3 * 2, HEIGHT);
+    divide(1, 1, width, height);
+  }
+
+  private boolean initialize(String input) {
     initializeTETile();
     long number = handleInput(input);
     if (number == -1) {
-      return;
+      return false;
     } else {
       random = new Random(number);
+      return true;
     }
   }
 
@@ -63,10 +97,11 @@ public class Engine {
     tetile = new TETile[WIDTH][HEIGHT];
     for (int i = 0; i < WIDTH; i++) {
       for (int j = 0; j < HEIGHT; j++) {
-        tetile[i][j] = Tileset.WALL;
+        tetile[i][j] = Tileset.NOTHING;
       }
     }
   }
+
   private long handleInput(String input) {
     String s;
     long res;
@@ -88,24 +123,60 @@ public class Engine {
     } else if (s.length() > 19) {
       return false;
     }
-    if (s.charAt(1) > '2') return false;
-    if (s.charAt(2) > '2') return false;
-    if (s.charAt(3) > '3') return false;
-    if (s.charAt(4) > '3') return false;
-    if (s.charAt(5) > '7') return false;
-    if (s.charAt(6) > '2') return false;
-    if (s.charAt(7) > '0') return false;
-    if (s.charAt(8) > '3') return false;
-    if (s.charAt(9) > '6') return false;
-    if (s.charAt(10) > '8') return false;
-    if (s.charAt(11) > '5') return false;
-    if (s.charAt(12) > '4') return false;
-    if (s.charAt(13) > '7') return false;
-    if (s.charAt(14) > '7') return false;
-    if (s.charAt(15) > '5') return false;
-    if (s.charAt(16) > '8') return false;
-    if (s.charAt(17) > '0') return false;
-    if (s.charAt(18) > '7') return false;
+    if (s.charAt(1) > '2') {
+      return false;
+    }
+    if (s.charAt(2) > '2') {
+      return false;
+    }
+    if (s.charAt(3) > '3') {
+      return false;
+    }
+    if (s.charAt(4) > '3') {
+      return false;
+    }
+    if (s.charAt(5) > '7') {
+      return false;
+    }
+    if (s.charAt(6) > '2') {
+      return false;
+    }
+    if (s.charAt(7) > '0') {
+      return false;
+    }
+    if (s.charAt(8) > '3') {
+      return false;
+    }
+    if (s.charAt(9) > '6') {
+      return false;
+    }
+    if (s.charAt(10) > '8') {
+      return false;
+    }
+    if (s.charAt(11) > '5') {
+      return false;
+    }
+    if (s.charAt(12) > '4') {
+      return false;
+    }
+    if (s.charAt(13) > '7') {
+      return false;
+    }
+    if (s.charAt(14) > '7') {
+      return false;
+    }
+    if (s.charAt(15) > '5') {
+      return false;
+    }
+    if (s.charAt(16) > '8') {
+      return false;
+    }
+    if (s.charAt(17) > '0') {
+      return false;
+    }
+    if (s.charAt(18) > '7') {
+      return false;
+    }
     return true;
   }
 
@@ -120,19 +191,82 @@ public class Engine {
     return s.toString();
   }
 
-
-  /**
-   * Method used for exploring a fresh world. This method should handle all inputs,
-   * including inputs from the main menu.
-   */
-  public void interactWithKeyboard() {
-    Menu menu = new Menu(WIDTH, HEIGHT);
-    String input = menu.freshAndGetString();
-    menu.close();
-    initialize(input);
+  private void createBound(int w, int h) {
+    for (int i = 0; i <= w; i++) {
+      tetile[i][0] = Tileset.WALL;
+      tetile[i][h] = Tileset.WALL;
+    }
+    for (int i = 0; i <= h; i++) {
+      tetile[0][i] = Tileset.WALL;
+      tetile[w][i] = Tileset.WALL;
+    }
   }
 
+  private boolean selectDirection(int w, int h) {
+    if (w == h) {
+      return random.nextBoolean();
+    }
+    return w < h;
+  }
+
+  private void divide(int x1, int y1, int x2, int y2) {
+    if (y2 - y1 <= 3 || x2 - x1 <= 3) {
+      return;
+    }
+    if (selectDirection(x2 - x1, y2 - y1)) {
+      int y = RandomUtils.uniform(random, y1 + 2, y2-1);
+      int x = horizontal(x1, x2, y);
+      ter.renderFrame(tetile);
+      divide(x1, y1, x, y);
+      divide(x, y1, x2, y);
+      divide(x1, y, x, y2);
+      divide(x, y, x2, y2);
+    } else {
+      int x = RandomUtils.uniform(random, x1 + 2, x2-1);
+      int y = vertical(y1, y2, x);
+      divide(x1, y1, x, y);
+      divide(x, y1, x2, y);
+      divide(x1, y, x, y2);
+      divide(x, y, x2, y2);
+    }
+  }
+
+  private int horizontal(int x1, int x2, int y) {
+    int startX = Math.min(x1, x2);
+    int endX = Math.max(x1, x2);
+    if (startX == endX) {
+      return -1;
+    }
+
+    for (int i = startX + 1; i < endX; i++) {
+      tetile[i][y] = Tileset.WALL;
+    }
+    int gap = RandomUtils.uniform(random, startX + 1, endX);
+    tetile[gap][y] = Tileset.NOTHING;
+    return gap;
+  }
+
+  private int vertical(int y1, int y2, int x) {
+    int startY = Math.min(y1, y2);
+    int endY = Math.max(y1, y2);
+    if (startY == endY) {
+      return -1;
+    }
+
+    for (int i = startY + 1; i < endY; i++) {
+      tetile[x][i] = Tileset.WALL;
+    }
+    int gap = RandomUtils.uniform(random, startY + 1, endY);
+    tetile[x][gap] = Tileset.NOTHING;
+    return gap;
+  }
+
+  private void bfs() {
+  }
+
+
   private class Menu {
+
     private int width;
     private int height;
 
@@ -160,6 +294,7 @@ public class Engine {
       StdDraw.text((double) width / 2, (double) height / 5 * 2, s);
       StdDraw.show();
     }
+
     public String freshAndGetString() {
       boolean flag = true;
       StringBuilder sb = new StringBuilder();
@@ -176,8 +311,18 @@ public class Engine {
       }
       return sb.toString();
     }
+
     public void close() {
     }
+  }
+  private class Position {
+    private int x;
+    private int y;
+    public Position(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
   }
 }
 
